5357b5da3b4d02f0afd61d265c9c4fa67f0cee7d
stylianos <chalkidis@leoparden.net>
2024-11-29 16:18:07 +0100

    style: format code

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Frontend components</title>
-    <link rel="stylesheet" href="build/styles.css "/>
+    <link rel="stylesheet" href="build/styles.css"/>
 </head>
 <body>
 <script type="text/javascript" src="build/app.js"></script>
